623c116c9d274ad19b06fb0031a7fcec5dd76a37
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2014-12-04 15:53:45 -0800

    Added a little function to make mdtraj topologies

---
+++ chemview/contrib.py
@@ -0,0 +1,14 @@
+'''Nice functions for integration'''
+import mdtraj as md
+
+def topology_mdtraj(traj):
+    '''Generate topology spec for the MolecularViewer from mdtraj'''
+    top = {}
+    top['atom_types'] = [a.element.symbol for a in traj.topology.atoms]
+    top['atom_names'] = [a.name for a in traj.topology.atoms]
+    top['bonds'] = [(a.index, b.index) for a, b in traj.topology.bonds]
+    top['secondary_structure'] = md.compute_dssp(traj[0])[0]
+    top['residue_types'] = [r.name for r in traj.topology.residues ]
+    top['residue_indices'] = [ [a.index for a in r.atoms] for r in traj.topology.residues ]
+
+    return top
