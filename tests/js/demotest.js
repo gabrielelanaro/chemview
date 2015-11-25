1b6944852d8ef86e5e8b37dff7e4192bebad4398
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2015-11-24 17:24:05 -0800

    Get some files to get picked up by setup.py

--- tests/js/demotest.js
+++ tests/js/demotest.js
@@ -1,23 +1,14 @@
 module.exports = {
-    after : function(browser) {
-        console.log('Closing down...');
-        browser.getLog('browser', function(result) {
-                                 console.log(result);
-                             }).end();
-    },
     "Test Fullscreen": function(browser){
         // control the browser
         open_notebook(browser);
         
         browser.restartKernel(2000)
                .executeCell(0)
-               .pause(1000);
-        
-        cellError(browser);       
-        
+               .pause(3000)
+               .cellHasError(0);
+    
         browser.waitForElementVisible(".ipy-widget.widget-container.widget-box", 10);
-        
-        browser.expect.element(".output_error").not.present;
         
         var CANVAS_SEL = ".ipy-widget.widget-container.widget-box canvas";
         browser.expect.element(CANVAS_SEL)
@@ -27,28 +18,10 @@ module.exports = {
         browser.moveToElement(CANVAS_SEL, 2, 2).doubleClick();
         browser.expect.element(CANVAS_SEL)
                .to.have.css("width").which.not.equals("300px");
+        browser.end();
 
     }
 }
-
-function cellError(browser) {
-    return browser.execute(function (cellNum) {
-        var cell = IPython.notebook.get_cell(0);
-        var out = cell.output_area.outputs[0];
-        if (out.output_type == 'error') {
-            return { evalue: out.evalue, ename: out.ename };
-        }
-        else {
-            return null;
-        }
-    },
-        
-    [0],
-        
-    function (result) {
-        console.log(result.value);
-    });
-};
 
 
 function open_notebook(browser) {
