c38ea5bd3fdb7eacbfaf5014b4c6dead0f0364bf
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2015-11-25 13:31:20 -0800

    Skip webgl tests on travis

--- tests/js/demotest.js
+++ tests/js/demotest.js
@@ -8,17 +8,19 @@ module.exports = {
                .pause(3000)
                .cellHasError(0);
         
-        browser.getLog(function (result) { console.log(result); });
-        browser.waitForElementVisible(".ipy-widget.widget-container.widget-box", 10);
+        if (! (process.env.CI == "true") ) {
+            browser.waitForElementVisible(".ipy-widget.widget-container.widget-box", 10);
+            
+            var CANVAS_SEL = ".ipy-widget.widget-container.widget-box canvas";
+            browser.expect.element(CANVAS_SEL)
+                   .to.have.css("width").which.equals("300px");
+                   
+            // Now we doubleclick on it
+            browser.moveToElement(CANVAS_SEL, 2, 2).doubleClick();
+            browser.expect.element(CANVAS_SEL)
+                   .to.have.css("width").which.not.equals("300px");
+        }
         
-        var CANVAS_SEL = ".ipy-widget.widget-container.widget-box canvas";
-        browser.expect.element(CANVAS_SEL)
-               .to.have.css("width").which.equals("300px");
-               
-        // Now we doubleclick on it
-        browser.moveToElement(CANVAS_SEL, 2, 2).doubleClick();
-        browser.expect.element(CANVAS_SEL)
-               .to.have.css("width").which.not.equals("300px");
         browser.end();
 
     }
