341384b60cc4dd52183bded0ef5e01b03b3cc661
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2015-11-24 16:46:44 -0800

    added print error python

--- tests/js/demotest.js
+++ tests/js/demotest.js
@@ -11,10 +11,11 @@ module.exports = {
         
         browser.restartKernel(2000)
                .executeCell(0)
-               .pause(5000).getLog('browser', function(result) {
-                                        console.log(result);
-                                    })
-               .waitForElementVisible(".ipy-widget.widget-container.widget-box", 10);
+               .pause(1000);
+        
+        cellError(browser);       
+        
+        browser.waitForElementVisible(".ipy-widget.widget-container.widget-box", 10);
         
         browser.expect.element(".output_error").not.present;
         
@@ -26,13 +27,28 @@ module.exports = {
         browser.moveToElement(CANVAS_SEL, 2, 2).doubleClick();
         browser.expect.element(CANVAS_SEL)
                .to.have.css("width").which.not.equals("300px");
-        
-        // browser.pause(1000).keys([browser.Keys.ESC]).pause(1000);
-        // browser.expect.element(CANVAS_SEL)
-        //        .to.have.css("width").which.equals("300px");
-        // browser.end();
+
     }
 }
+
+function cellError(browser) {
+    return browser.execute(function (cellNum) {
+        var cell = IPython.notebook.get_cell(0);
+        var out = cell.output_area.outputs[0];
+        if (out.output_type == 'error') {
+            return { evalue: out.evalue, ename: out.ename };
+        }
+        else {
+            return null;
+        }
+    },
+        
+    [0],
+        
+    function (result) {
+        console.log(result.value);
+    });
+};
 
 
 function open_notebook(browser) {
