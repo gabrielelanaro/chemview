88044ff6ef80e17bb9ea06093f1af2cadd076849
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2015-12-01 12:39:06 -0800

    Update viewer.py

--- chemview/viewer.py
+++ chemview/viewer.py
@@ -262,7 +262,7 @@ class MolecularViewer(RepresentationViewer):
         if isolevel >= 0:
             triangles = marching_cubes(data, isolevel)
         else:
-            triangle = marchingcubes(-data, -isolevel)
+            triangle = marching_cubes(-data, -isolevel)
         faces = []
         verts = []
         for i, t in enumerate(triangles):
