a9d98c634b0d63c94b6e8e1b6a8aca882e604753
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2016-02-15 16:55:17 -0800

    Badass validation system

--- chemview/scene.py
+++ chemview/scene.py
@@ -1,5 +1,7 @@
 '''Scene validation utilities'''
 import operator
+import numpy as np
+import six
 
 class Validator(object):
     
@@ -9,15 +11,19 @@ class Validator(object):
     def cast(self, value):
         pass
 
+    def default(self, inp=None):
+        return self._default
+
 
 class BoundedScalar(Validator):
     
-    def __init__(self, start, end, type, ginclusive=True, linclusive=True):
+    def __init__(self, start, end, type, ginclusive=True, linclusive=True, default=None):
         self.start = start
         self.end = end
         self.ginclusive = ginclusive
         self.linclusive = linclusive
         self.type = type
+        self._default = default
         
     def validate(self, value):
         lo = operator.le if self.linclusive else operator.lt
@@ -28,11 +34,24 @@ class BoundedScalar(Validator):
     def cast(self, value):
         return self.type(value)
 
-class ListOf(Validator):
+class TypedList(Validator):
     
-    def __init__(self, type, length=None):
+    def __init__(self, type, length=None, match_length=None, default_item=None, default=None):
         self.type = type
         self.length = length
+        self.match_length = match_length
+        self.default_item = default_item
+        self._default = default
+    
+    
+    def default(self, inp=None):
+        if self._default is not None:
+            return self._default
+             
+        if self.match_length is not None and self.default_item is not None:
+            return [self.default_item] * len(inp[self.match_length])
+        else:
+            raise ValueError("This field is required")
     
     def validate(self, value):
         # We check the first element only
@@ -49,44 +68,190 @@ class ListOf(Validator):
         return [self.type(t) for v in value]
 
 
+class UniqueID(Validator):
+    def default(self, inp=None):
+        raise ValueError("No default for this field")
+        
+    def validate(self, value):
+        return isinstance(value, int), 'UUid not valid'
+        
+    def cast(self, value):
+        return value
+    
+class Array(Validator):
+    def __init__(self, shape, type):
+        self.shape = shape
+        self.type = type
 
+    def default(self, inp=None):
+        raise ValueError("No default for this field")
+        
+    def validate(self, value):
+        return True, ''
+        
+    def cast(self, value):
+        return np.array(value, dtype=self.type)
 
+class Keyword(Validator):
+    def __init__(self, value):
+        self.value = value
+    
+    def default(self, inp=None):
+        raise ValueError("No default for this field")
+    
+    def validate(self, value):
+        return value == self.value, 'should be {}'.format(self.value)
+    
+    def cast(self, value):
+        return six.u(value)
+
+class MatchSchema(object):
+
+    def __init__(self, matchers, field):
+        self.field = field
+        self.matchers = {m[field].value: m for m in matchers}
+
+    def propagate(self, value):
+        return self.matchers[value[self.field]]
+        
 class ValidationError(Exception):
     pass
 
+from collections import OrderedDict
 
-# TODO: we can use a default value
-CAMERA_TEMPLATE = {'aspect': (1.0, BoundedScalar(0.0, float('inf'), float, ginclusive=False)),
-                   'vfov': (90.0, BoundedScalar(0.0, 180.0, float)),
-                   'location': ([0.0, 0.0, -1.0], ListOf(float, 3)),
-                   'quaternion': ([0.0, 0.0, 0.0, 1.0], ListOf(float, 3)),
-                   'target': ([0.0, 0.0, 0.0], ListOf(float, 3))}
-                   
-                   
-# POINTS_OPTIONS_TEMPLATE = {'coordinates': (None, ArrayOf(shape=(-1, 3), type=np.float32))}
+POINTS_SCHEMA = { 
+    "rep_id": UniqueID(),
+    "rep_type": Keyword("points"),
+    "options":  
+        OrderedDict({'coordinates': Array(shape=(-1, 3), type=np.float32),
+                     'colors': TypedList(int, match_length='coordinates', default_item=0xffffff),
+                     'sizes': TypedList(float, match_length='coordinates', default_item=1.0),
+                     'visible': TypedList(float, match_length='coordinates', default_item=1.0)})
+}
+
+SPHERES_SCHEMA = { 
+    "rep_id": UniqueID(),
+    "rep_type": Keyword("spheres"),
+    "options" : 
+        OrderedDict({'coordinates': Array(shape=(-1, 3), type=np.float32),
+                     'colors': TypedList(int, match_length='coordinates', default_item=0xffffff),
+                     'radii': TypedList(float, match_length='coordinates', default_item=1.0),
+                     'alpha': TypedList(float, match_length='coordinates', default_item=1.0)})
+}
+
+
+CYLINDERS_SCHEMA = { 
+    "rep_id": UniqueID(),
+    "rep_type": Keyword("cylinders"),
+    "options" : 
+        OrderedDict({'startCoords': Array(shape=(-1, 3), type=np.float32),
+                     'endCoords': Array(shape=(-1, 3), type=np.float32),
+                     'colors': TypedList(int, match_length='startCoords', default_item=0xffffff),
+                     'radii': TypedList(float, match_length='startCoords', default_item=1.0),
+                     'alpha': TypedList(float, match_length='startCoords', default_item=1.0)})
+}
+
+CYLINDERS_SCHEMA = { 
+    "rep_id": UniqueID(),
+    "rep_type": Keyword("cylinders"),
+    "options" : 
+        OrderedDict({'startCoords': Array(shape=(-1, 3), type=np.float32),
+                     'endCoords': Array(shape=(-1, 3), type=np.float32),
+                     'colors': TypedList(int, match_length='startCoords', default_item=0xffffff),
+                     'radii': TypedList(float, match_length='startCoords', default_item=1.0),
+                     'alpha': TypedList(float, match_length='startCoords', default_item=1.0)})
+}
+
+LINES_SCHEMA = { 
+    "rep_id": UniqueID(),
+    "rep_type": Keyword("lines"),
+    "options" : 
+        OrderedDict({'startCoords': Array(shape=(-1, 3), type=np.float32),
+                     'endCoords': Array(shape=(-1, 3), type=np.float32),
+                     'startColors': TypedList(int, match_length='startCoords', default_item=0xffffff),
+                     'endColors': TypedList(float, match_length='endCoords', default_item=1.0)})
+}
+
+SURFACE_SCHEMA = { 
+    "rep_id": UniqueID(),
+    "rep_type": Keyword("surface"),
+    "options" : 
+        OrderedDict({'verts': Array(shape=(-1, 3), type=np.float32),
+                     'faces': Array(shape=(-1, 3), type=np.uint32),
+                     'colors': TypedList(int, match_length='verts', default_item=0xffffff),
+                     'alpha': TypedList(float, match_length='verts', default_item=1.0)})
+}
+
+SMOOTHTUBE_SCHEMA = {
+    "rep_id": UniqueID(),
+    "rep_type": Keyword("smoothtube"),
+    "options" : 
+    OrderedDict({'coordinates': Array(shape=(-1, 3), type=np.float32),
+                 'radius': BoundedScalar(0.0, float('inf'), float, default=1.0),
+                 'color': BoundedScalar(0, 0xffffff, int, default=0xffffff),
+                 'resolution': BoundedScalar(2, float('inf'), int, default=16)})
+}
+
+
+SCHEMA = {
+    'camera' : {'aspect': BoundedScalar(0.0, float('inf'), float, default=1.0, ginclusive=False),
+                'vfov': BoundedScalar(0.0, 180.0, float, default=90.0),
+                'location': TypedList(float, 3, default=[0.0, 0.0, -1.0]),
+                'quaternion': TypedList(float, 4, default=[0.0, 0.0, 0.0, 1.0]),
+                'target': TypedList(float, 3, default=[0.0, 0.0, 0.0])},
+                
+    'representations' : MatchSchema([POINTS_SCHEMA, 
+                                     SPHERES_SCHEMA, 
+                                     LINES_SCHEMA, 
+                                     CYLINDERS_SCHEMA,
+                                     SURFACE_SCHEMA,
+                                     SMOOTHTUBE_SCHEMA], 'rep_type')
+}
+                    
+
+def validate_schema(value, schema):
+    retval = {}
+    for key, validator in schema.items():
+        
+        if isinstance(validator, dict):
+            inp = value.get(key, {}).copy()
+            validated_inp = validate_schema(inp, validator)
+            retval[key] = validated_inp
+        
+        elif isinstance(validator, MatchSchema):
+            inp = value.get(key, []).copy()
+            
+            validated_inp = []
+            for val in inp:
+                sub_schema = validator.propagate(val)
+                validated_inp.append(validate_schema(val, sub_schema))
+                
+            retval[key] = validated_inp
+            
+        
+        elif isinstance(validator, Validator):
+            if key not in value:
+                try:
+                    retval[key] = validator.default(value)
+                except ValueError:
+                    raise ValidationError('Problem: "{}" is required.'.format(key))
+            else:
+                try:
+                    cast = validator.cast(value[key])
+                except ValueError:
+                    raise ValidationError('Problem: "{}" is {}, cannot cast'.format(key, value[key]))
+                
+                ok, msg = validator.validate(cast)
+                if not ok:
+                    raise ValidationError('Problem: "{}" is {} but {}'.format(key, value[key], msg))
+                retval[key] = cast
+        else:
+            raise ValueError("schema not valid")
+        
+    return retval
+    
 def normalize_scene(scene):
     """Normalize incomplete scene with sane defaults"""
     retval = scene.copy()
-    camera = scene.get('camera', {}).copy()
     
-    for key, (default_value, validator) in CAMERA_TEMPLATE.items():
-        if key not in camera:
-            camera[key] = default_value
-        else:
-            value = camera[key]
-            
-            try:
-                cast = validator.cast(value)
-            except ValueError:
-                raise ValidationError('camera problem: "{}" is {}, cannot cast'.format(key, value))
-                
-                
-            ok, msg = validator.validate(cast)
-            if not ok:
-                raise ValidationError('camera problem: "{}" is {} but {}'.format(key, value, msg))
-                
-            camera[key] = cast
-    
-    scene['camera'] = camera
-    
-    return scene
+    return validate_schema(scene, SCHEMA)
--- tests/test_scene.py
+++ tests/test_scene.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import numpy as np
 from nose.tools import eq_, assert_raises
-from chemview.scene import normalize_scene, ValidationError
+from chemview.scene import normalize_scene, ValidationError, POINTS_SCHEMA, SPHERES_SCHEMA, validate_schema
 
 def test_normalize_scene():
     coordinates = np.array([[0, 0, 0], [0, 0, 1], [0, 1, 2]], dtype='float32')
@@ -17,9 +17,32 @@ def test_normalize_scene():
     eq_(nscene['camera']['quaternion'], [0, 0, 0, 1])
     eq_(nscene['camera']['target'], [0, 0, 0])
 
-    
+    eq_(nscene['representations'][0]['options']['colors'], [0xffffff, 0xffffff, 0xffffff])
+
 def test_camera_validation():
     scene = {'camera': {"aspect" : -1}}
     
     with assert_raises(ValidationError):
         nscene = normalize_scene(scene)
+
+def test_points_validation():
+    coordinates = np.array([[0, 0, 0], [0, 0, 1], [0, 1, 2]], dtype='float32')
+    val = {"rep_id" : 1,
+           "rep_type" : 'points',
+           "options" : {"coordinates": coordinates}}
+     
+    nschema = validate_schema(val, POINTS_SCHEMA)
+    eq_(nschema['options']['colors'], [0xffffff, 0xffffff, 0xffffff])
+    eq_(nschema['options']['sizes'], [1.0, 1.0, 1.0])
+
+
+def test_spheres_validation():
+    coordinates = np.array([[0, 0, 0], [0, 0, 1], [0, 1, 2]], dtype='float32')
+    val = {"rep_id" : 1,
+           "rep_type" : 'spheres',
+           "options" : {"coordinates": coordinates}}
+    
+    nschema = validate_schema(val, SPHERES_SCHEMA)
+    eq_(nschema['options']['colors'], [0xffffff, 0xffffff, 0xffffff])
+    eq_(nschema['options']['radii'], [1.0, 1.0, 1.0])
+    
